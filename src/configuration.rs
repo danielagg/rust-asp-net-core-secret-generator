a578eed7c10e96d17673a5da5bbc5ba2bfde4008
Daniel Agg <daniel.agg@outlook.com>
2023-04-26 22:20:08 +0200

    get secrets to be injected

--- src/configuration.rs
+++ src/configuration.rs
@@ -3,34 +3,63 @@ use std::fmt;
 
 #[derive(Debug)]
 pub struct Configuration {
-    pub tenant_id: String,
-    pub client_id: String,
-    pub client_secret: String,
-    pub subscription_id: String,
-    pub resource_group_name: String,
-    pub key_vault_name: String,
+    pub azure_tenant_id: String,
+    pub azure_client_id: String,
+    pub azure_client_secret: String,
+    pub azure_subscription_id: String,
+    pub azure_resource_group_name: String,
+    pub azure_key_vault_name: String,
+    pub azure_devops_org: String,
+    pub azure_devops_proj: String,
+    pub azure_devops_repository_id: String,
+    pub azure_devops_email: String,
+    pub azure_devops_personal_access_token: String,
+    pub appsettings_json_path: String,
 }
 
 impl fmt::Display for Configuration {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Configuration:\n")?;
-        write!(f, "  Tenant ID: {}\n", self.tenant_id)?;
-        write!(f, "  Client ID: {}\n", self.client_id)?;
-        write!(f, "  Client Secret: {}\n", self.client_secret)?;
-        write!(f, "  Subscription ID: {}\n", self.subscription_id)?;
-        write!(f, "  Resource Group Name: {}\n", self.resource_group_name)?;
-        write!(f, "  Key Vault Name: {}", self.key_vault_name)
+        write!(f, "Azure Tenant ID: {}\n", self.azure_tenant_id)?;
+        write!(f, "Azure Client ID: {}\n", self.azure_client_id)?;
+        write!(f, "Azure Client Secret: {}\n", self.azure_client_secret)?;
+        write!(f, "Azure Subscription ID: {}\n", self.azure_subscription_id)?;
+        write!(
+            f,
+            "Azure Resource Group Name: {}\n",
+            self.azure_resource_group_name
+        )?;
+        write!(f, "Azure Key Vault Name: {}", self.azure_key_vault_name)?;
+        write!(f, "Azure DevOps Organization: {}", self.azure_devops_org)?;
+        write!(f, "Azure DevOps Project: {}", self.azure_devops_proj)?;
+        write!(
+            f,
+            "Azure DevOps Repository ID: {}",
+            self.azure_devops_repository_id
+        )?;
+        write!(f, "Azure DevOps Email: {}", self.azure_devops_email)?;
+        write!(
+            f,
+            "Azure DevOps Personal Access Token: {}",
+            self.azure_devops_personal_access_token
+        )?;
+        write!(f, "AppSettings JSON Path: {}", self.appsettings_json_path)
     }
 }
 
 pub fn get_configuration() -> Configuration {
     let res = Configuration {
-        tenant_id: env::var("AZURE_TENANT_ID").unwrap(),
-        client_id: env::var("AZURE_CLIENT_ID").unwrap(),
-        client_secret: env::var("AZURE_CLIENT_SECRET").unwrap(),
-        subscription_id: env::var("SUBSCRIPTION_ID").unwrap(),
-        resource_group_name: env::var("RESOURCE_GROUP_NAME").unwrap(),
-        key_vault_name: env::var("KEY_VAULT_NAME").unwrap(),
+        azure_tenant_id: env::var("AZURE_TENANT_ID").unwrap(),
+        azure_client_id: env::var("AZURE_CLIENT_ID").unwrap(),
+        azure_client_secret: env::var("AZURE_CLIENT_SECRET").unwrap(),
+        azure_subscription_id: env::var("AZURE_SUBSCRIPTION_ID").unwrap(),
+        azure_resource_group_name: env::var("AZURE_RESOURCE_GROUP_NAME").unwrap(),
+        azure_key_vault_name: env::var("AZURE_KEY_VAULT_NAME").unwrap(),
+        azure_devops_org: env::var("AZURE_DEVOPS_ORGANIZATION").unwrap(),
+        azure_devops_proj: env::var("AZURE_DEVOPS_PROJECT").unwrap(),
+        azure_devops_repository_id: env::var("AZURE_DEVOPS_REPO_ID").unwrap(),
+        azure_devops_email: env::var("AZURE_DEVOPS_EMAIL").unwrap(),
+        azure_devops_personal_access_token: env::var("AZURE_DEVOPS_PERSONAL_ACCESS_TOKEN").unwrap(),
+        appsettings_json_path: env::var("APPSETTINGS_JSON_PATH").unwrap(),
     };
 
     return res;
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use anyhow::anyhow;
 use configuration::{get_configuration, Configuration};
 use dotenv::dotenv;
 use reqwest::{self, header};
+use serde_json::{Map, Value};
 use tokio;
 
 #[tokio::main]
@@ -11,38 +12,66 @@ async fn main() -> anyhow::Result<()> {
     dotenv().ok();
 
     let config = get_configuration();
-    let access_token = get_access_token(&config).await?;
 
-    let url = format!(
-        "https://management.azure.com/subscriptions/{}/resourceGroups/{}/providers/Microsoft.KeyVault/vaults/{}?api-version=2022-07-01",
-        config.subscription_id,
-        config.resource_group_name,
-        config.key_vault_name);
+    let appsettings = get_appsettings_content(&config).await?;
+    let secrets_to_fetch = convert_appsettings_entries_to_keyvault_secret_names(&appsettings);
+
+    let azure_management_access_token = get_azure_management_access_token(&config).await?;
+
+    // let url = format!(
+    //     "https://management.azure.com/subscriptions/{}/resourceGroups/{}/providers/Microsoft.KeyVault/vaults/{}?api-version=2022-07-01",
+    //     config.subscription_id,
+    //     config.resource_group_name,
+    //     config.key_vault_name);
+
+    // let client = reqwest::Client::new();
+    // let response = client
+    //     .get(url.as_str())
+    //     .header(header::AUTHORIZATION, format!("Bearer {}", access_token))
+    //     .send()
+    //     .await?;
+
+    // let resp = response.text().await?;
+
+    // println!("{}", resp);
+    Ok(())
+}
+
+async fn get_appsettings_content(config: &Configuration) -> anyhow::Result<String> {
+    let devops_url = format!(
+        "https://dev.azure.com/{}/{}/_apis/git/repositories/{}/items?path={}&api-version=6.0",
+        config.azure_devops_org,
+        config.azure_devops_proj,
+        config.azure_devops_repository_id,
+        config.appsettings_json_path
+    );
 
     let client = reqwest::Client::new();
     let response = client
-        .get(url.as_str())
-        .header(header::AUTHORIZATION, format!("Bearer {}", access_token))
+        .get(devops_url.as_str())
+        .basic_auth(
+            config.azure_devops_email.clone(),
+            Some(config.azure_devops_personal_access_token.clone()),
+        )
         .send()
         .await?;
 
     let resp = response.text().await?;
 
-    println!("{}", resp);
-    Ok(())
+    Ok(resp)
 }
 
-async fn get_access_token(config: &Configuration) -> anyhow::Result<String> {
+async fn get_azure_management_access_token(config: &Configuration) -> anyhow::Result<String> {
     let body = format!(
         "grant_type=client_credentials&client_id={}&client_secret={}&resource=https://management.azure.com",
-        config.client_id, config.client_secret
+        config.azure_client_id, config.azure_client_secret
     );
 
     let client = reqwest::Client::new();
     let response = client
         .post(&format!(
             "https://login.microsoftonline.com/{}/oauth2/token",
-            config.tenant_id
+            config.azure_tenant_id
         ))
         .header(header::CONTENT_TYPE, "application/x-www-form-urlencoded")
         .body(body)
@@ -57,4 +86,48 @@ async fn get_access_token(config: &Configuration) -> anyhow::Result<String> {
         .ok_or_else(|| anyhow!("access_token not found"))?;
 
     return Ok(access_token.to_owned());
+}
+
+fn convert_appsettings_entries_to_keyvault_secret_names(json_str: &str) -> Vec<String> {
+    let json: Value = serde_json::from_str(json_str).expect("Failed to parse JSON");
+
+    let mut result: Vec<String> = Vec::new();
+
+    populate_name_of_secrets_to_be_injected(&json, None, &mut result);
+
+    return result;
+}
+
+fn populate_name_of_secrets_to_be_injected(
+    json: &Value,
+    prefix: Option<&str>,
+    result: &mut Vec<String>,
+) {
+    match json {
+        Value::Null | Value::Bool(_) | Value::Number(_) => {
+            if let Some(key) = prefix {
+                result.push(key.to_string());
+            }
+        }
+        Value::String(s) => {
+            if let Some(key) = prefix {
+                if s == "{InjectedFromKeyvaultDuringRelease}" {
+                    result.push(key.to_string());
+                }
+            }
+        }
+        Value::Array(arr) => {
+            for (i, item) in arr.iter().enumerate() {
+                let key = prefix.map_or_else(|| i.to_string(), |p| format!("{}-{}", p, i));
+                populate_name_of_secrets_to_be_injected(item, Some(&key), result);
+            }
+        }
+        Value::Object(obj) => {
+            for (key, value) in obj.iter() {
+                let new_prefix =
+                    prefix.map_or_else(|| key.to_string(), |p| format!("{}-{}", p, key));
+                populate_name_of_secrets_to_be_injected(value, Some(&new_prefix), result);
+            }
+        }
+    }
 }
